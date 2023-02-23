cbd5f4b615162f454a478d9222d677b616d6f58e
xunto <xunto.orlov@gmail.com>
2023-02-23 21:20:58 +0300

    Calculate sequences.

--- src/sanger/assignment/nucleotides.py
+++ src/sanger/assignment/nucleotides.py
@@ -1,7 +1,9 @@
+from pprint import pprint
 from typing import Iterator
 
 from fastq import fastq_object
 
 
 def count_nucleotides(fq: Iterator[fastq_object]) -> int:
+    pprint(list(fq))
     return 0
--- src/sanger/assignment/sequences.py
+++ src/sanger/assignment/sequences.py
@@ -1,7 +1,15 @@
-from typing import Iterator
+from typing import Iterator, Any
 
 from fastq import fastq_object
 
 
+def _iterator_length(iterator: Iterator[Any]):
+    """Space efficient way of calculating iterator length."""
+
+    return sum(1 for _ in iterator)
+
+
 def count_sequences(fq: Iterator[fastq_object]) -> int:
-    return 0
+    """Count sequences in fastq data."""
+
+    return _iterator_length(fq)
