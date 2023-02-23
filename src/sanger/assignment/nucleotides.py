e23f6bc0d69beb9acc536d8bb0340edfab9090a7
xunto <xunto.orlov@gmail.com>
2023-02-23 21:32:46 +0300

    Count nucleotides.py

--- src/sanger/assignment/nucleotides.py
+++ src/sanger/assignment/nucleotides.py
@@ -1,9 +1,11 @@
-from pprint import pprint
 from typing import Iterator
 
 from fastq import fastq_object
 
 
 def count_nucleotides(fq: Iterator[fastq_object]) -> int:
-    pprint(list(fq))
-    return 0
+    """Count nucleotides in fastq data."""
+
+    # NOTE: Generator is processing 1 item at a time,
+    # so there is constant memory usage.
+    return sum(len(v.body) for v in fq)
